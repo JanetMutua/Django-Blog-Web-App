cb4a13791372718c7c4e29f9d020ab6fb030894b
JanetMutua <janetmutua04@gmail.com>
2021-10-13 08:20:16 +0300

    Added html code

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {'author': 'Mark',
+    'title': 'BMS Builder',
+    'content': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, assumenda nemo eveniet libero ab molestiae, sunt animi aperiam quibusdam provident fugiat voluptates quis earum. Minima quo perferendis animi beatae repellat!',
+    'date': '12/01/2020'},
+
+    {'author': 'Jean',
+    'title': 'Commercial Setup Teams',
+    'content': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, assumenda nemo eveniet libero ab molestiae, sunt animi aperiam quibusdam provident fugiat voluptates quis earum. Minima quo perferendis animi beatae repellat!',
+    'date': '01/02/2020'}
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Blog Home Page</h1>')
+    context ={'posts': posts}
+    return render(request,'blog/home.html',context)
 
 def about(request):
-    return HttpResponse('<h1>About Page</>')
+    return render(request, 'blog/about.html', {'title':'About'})
