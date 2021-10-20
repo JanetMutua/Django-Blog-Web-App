0d08588f9fe662287bc5763e2e3a197707e64c70
JanetMutua <janetmutua04@gmail.com>
2021-10-20 13:53:46 +0300

    Configuring SQLite

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,23 @@
 from django.shortcuts import render
+from .models import Post
 
 posts = [
     {'author': 'Mark',
-    'title': 'BMS Builder',
-    'content': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, assumenda nemo eveniet libero ab molestiae, sunt animi aperiam quibusdam provident fugiat voluptates quis earum. Minima quo perferendis animi beatae repellat!',
-    'date': '12/01/2020'},
+     'title': 'BMS Builder',
+     'content': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, assumenda nemo eveniet libero ab molestiae, sunt animi aperiam quibusdam provident fugiat voluptates quis earum. Minima quo perferendis animi beatae repellat!',
+     'date': '12/01/2020'},
 
     {'author': 'Jean',
-    'title': 'Commercial Setup Teams',
-    'content': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, assumenda nemo eveniet libero ab molestiae, sunt animi aperiam quibusdam provident fugiat voluptates quis earum. Minima quo perferendis animi beatae repellat!',
-    'date': '01/02/2020'}
+     'title': 'Commercial Setup Teams',
+     'content': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, assumenda nemo eveniet libero ab molestiae, sunt animi aperiam quibusdam provident fugiat voluptates quis earum. Minima quo perferendis animi beatae repellat!',
+     'date': '01/02/2020'}
 ]
 
 
 def home(request):
-    context ={'posts': posts}
-    return render(request,'blog/home.html',context)
+    context = {'posts': Post.objects.all()}
+    return render(request, 'blog/home.html', context)
+
 
 def about(request):
-    return render(request, 'blog/about.html', {'title':'About'})
+    return render(request, 'blog/about.html', {'title': 'About'})
