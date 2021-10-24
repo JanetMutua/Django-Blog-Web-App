2cdb7b149b182b4f07dcebf687f1ca744f6ac549
JanetMutua <janetmutua04@gmail.com>
2021-10-24 14:38:39 +0300

    post creation module

--- Redbitblog/urls.py
+++ Redbitblog/urls.py
@@ -25,7 +25,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
-    path('', blog_views.home, name='blog-home'),
+    path('', blog_views.PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', blog_views.PostDetailView.as_view(), name='post-detail'),
     path('about/', blog_views.about, name='blog-about'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
