bd1990361ccb744b9e7b3928c153a184184239b3
JanetMutua <janetmutua04@gmail.com>
2022-01-23 12:48:14 +0300

    Refactor(models&views):refactored code in models and views

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,16 @@
 from django.db import models
+from django.db.models.fields import TextField
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from PIL import Image
 
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
+    picture = models.ImageField(
+        default='default-img.jpg', upload_to='profile_pics')
+    overview = models.CharField(max_length=100, null=True)
     content = models.TextField()
     date = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(
@@ -16,3 +21,11 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        img = Image.open(self.picture.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.picture.path)
